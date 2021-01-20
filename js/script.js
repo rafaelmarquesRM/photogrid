be90472d7c5d18325dece086abac377257ed700e
Rafael Marques <rafaelr.marques98@hotmail.com>
2021-01-20 20:26:25 -0300

    Função de curtir publicações

--- js/script.js
+++ js/script.js
@@ -1,14 +1,169 @@
-let gridPost = document.querySelector('.grid-post')
-let postLike = document.querySelector('.grid-description') 
-let liked = false
+let gridPost1 = document.querySelector('#grid-post1')
+let gridPost2 = document.querySelector('#grid-post2')
+let gridPost3 = document.querySelector('#grid-post3')
+let gridPost4 = document.querySelector('#grid-post4')
+let gridPost5 = document.querySelector('#grid-post5')
+let gridPost6 = document.querySelector('#grid-post6')
+let gridPost7 = document.querySelector('#grid-post7')
+let gridPost8 = document.querySelector('#grid-post8')
+let gridPost9 = document.querySelector('#grid-post9')
+let gridPost10 = document.querySelector('#grid-post10')
+let gridPost11 = document.querySelector('#grid-post11')
+let gridPost12 = document.querySelector('#grid-post12')
+let postLike1 = document.querySelector('#liked1')
+let postLike2 = document.querySelector('#liked2')
+let postLike3 = document.querySelector('#liked3')
+let postLike4 = document.querySelector('#liked4')
+let postLike5 = document.querySelector('#liked5')
+let postLike6 = document.querySelector('#liked6')
+let postLike7 = document.querySelector('#liked7')
+let postLike8 = document.querySelector('#liked8')
+let postLike9 = document.querySelector('#liked9')
+let postLike10 = document.querySelector('#liked10')
+let postLike11 = document.querySelector('#liked11')
+let postLike12 = document.querySelector('#liked12')
+let liked1 = false
+let liked2 = false
+let liked3 = false
+let liked4 = false
+let liked5 = false
+let liked6 = false
+let liked7 = false
+let liked8 = false
+let liked9 = false
+let liked10 = false
+let liked11 = false
+let liked12 = false
 
-function deuLike() {
-  if (liked == false) {
-    postLike.innerHTML = '💗 lorem picsum'
-    liked = true
+
+gridPost1.addEventListener('click', ()=> {
+  if (liked1 == false) {
+    postLike1.innerHTML = '💗'
+    liked1 = true
   }
-  else if (liked == true) {
-    postLike.innerHTML = '♡ lorem picsum'
-    liked = false
+  else if (liked1 == true) {
+    postLike1.innerHTML = '♡'
+    liked1 = false
   }
-}
+})
+
+gridPost2.addEventListener('click', ()=> {
+  if (liked2 == false) {
+    postLike2.innerHTML = '💗'
+    liked2 = true
+  }
+  else if (liked2 == true) {
+    postLike2.innerHTML = '♡'
+    liked2 = false
+  }
+})
+
+gridPost3.addEventListener('click', ()=> {
+  if (liked3 == false) {
+    postLike3.innerHTML = '💗'
+    liked3 = true
+  }
+  else if (liked3 == true) {
+    postLike3.innerHTML = '♡'
+    liked3 = false
+  }
+})
+
+gridPost4.addEventListener('click', ()=> {
+  if (liked4 == false) {
+    postLike4.innerHTML = '💗'
+    liked4 = true
+  }
+  else if (liked4 == true) {
+    postLike4.innerHTML = '♡'
+    liked4 = false
+  }
+})
+
+gridPost5.addEventListener('click', ()=> {
+  if (liked5 == false) {
+    postLike5.innerHTML = '💗'
+    liked5 = true
+  }
+  else if (liked5 == true) {
+    postLike5.innerHTML = '♡'
+    liked5 = false
+  }
+})
+
+gridPost6.addEventListener('click', ()=> {
+  if (liked6 == false) {
+    postLike6.innerHTML = '💗'
+    liked6 = true
+  }
+  else if (liked6 == true) {
+    postLike6.innerHTML = '♡'
+    liked6 = false
+  }
+})
+
+gridPost7.addEventListener('click', ()=> {
+  if (liked7 == false) {
+    postLike7.innerHTML = '💗'
+    liked7 = true
+  }
+  else if (liked7 == true) {
+    postLike7.innerHTML = '♡'
+    liked7 = false
+  }
+})
+
+gridPost8.addEventListener('click', ()=> {
+  if (liked8 == false) {
+    postLike8.innerHTML = '💗'
+    liked8 = true
+  }
+  else if (liked8 == true) {
+    postLike8.innerHTML = '♡'
+    liked8 = false
+  }
+})
+
+gridPost9.addEventListener('click', ()=> {
+  if (liked9 == false) {
+    postLike9.innerHTML = '💗'
+    liked9 = true
+  }
+  else if (liked9 == true) {
+    postLike9.innerHTML = '♡'
+    liked9 = false
+  }
+})
+
+gridPost10.addEventListener('click', ()=> {
+  if (liked10 == false) {
+    postLike10.innerHTML = '💗'
+    liked10 = true
+  }
+  else if (liked10 == true) {
+    postLike10.innerHTML = '♡'
+    liked10 = false
+  }
+})
+
+gridPost11.addEventListener('click', ()=> {
+  if (liked11 == false) {
+    postLike11.innerHTML = '💗'
+    liked11 = true
+  }
+  else if (liked11 == true) {
+    postLike11.innerHTML = '♡'
+    liked11 = false
+  }
+})
+
+gridPost12.addEventListener('click', ()=> {
+  if (liked12 == false) {
+    postLike12.innerHTML = '💗'
+    liked12 = true
+  }
+  else if (liked12 == true) {
+    postLike12.innerHTML = '♡'
+    liked12 = false
+  }
+})
